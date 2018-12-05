b991a5be176bffef7924f53c0fdc52edeb34da53
Fitz <fitz.wen@edragonconnect.com>
2018-12-05 12:39:36 +0800

    update

---
+++ static/pwa/precache-manifest.3c6885ee7d47451f31ff7edfd48c2cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "741121e579788f399f0c",
+    "url": "./pwa/static/js/runtime~main.741121e5.js"
+  },
+  {
+    "revision": "01397f038f39394cdb72",
+    "url": "./pwa/static/js/main.01397f03.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "./pwa/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "01397f038f39394cdb72",
+    "url": "./pwa/static/css/main.799b1836.chunk.css"
+  },
+  {
+    "revision": "11d1c8e15bbcb2c2d8af8f657b8fe227",
+    "url": "./pwa/index.html"
+  }
+];
--- static/pwa/service-worker.js
+++ static/pwa/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1bd5bc08a8c71109725d575ef86cd7a.js"
+  "./pwa/precache-manifest.3c6885ee7d47451f31ff7edfd48c2cab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./pwa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
