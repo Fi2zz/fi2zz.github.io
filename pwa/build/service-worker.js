24d6fdfb7102689ae838ae2a5e72c434fead9b6c
Fitz <fitz.wen@edragonconnect.com>
2018-12-05 11:51:59 +0800

    update

--- pwa/build/service-worker.js
+++ pwa/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1bd5bc08a8c71109725d575ef86cd7a.js"
+  "/pwa/precache-manifest.fec44fd307e85f15691cc3b70a3dd223.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pwa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
