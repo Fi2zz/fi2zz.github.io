95dd09eb221773f138f1a57441fb323dc7a5f87d
Fitz <fitz.wen@edragonconnect.com>
2018-12-05 11:50:11 +0800

    update

---
+++ pwa/build/precache-manifest.f1bd5bc08a8c71109725d575ef86cd7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13ee071854df4f596147",
+    "url": "/static/js/main.13ee0718.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "13ee071854df4f596147",
+    "url": "/static/css/main.799b1836.chunk.css"
+  },
+  {
+    "revision": "6f315de5619ab0a2c9d0f497c93a1bb0",
+    "url": "/index.html"
+  }
+];
